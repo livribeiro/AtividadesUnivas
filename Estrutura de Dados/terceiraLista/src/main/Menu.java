aaff5d8b115a4144fcdd4b9510828f219642f0b3
Livia Silva <38299821+livribeiro@users.noreply.github.com>
2020-07-02 21:21:35 -0300

    Create Menu.java

---
+++ Estrutura de Dados/terceiraLista/src/main/Menu.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Menu {
+	
+	public static final int PLAY 		= 1;
+	public static final int PAUSE 		= 3;
+	public static final int FORWARD  	= 4;
+	public static final int REWIND	 	= 5;
+	
+	
+}
