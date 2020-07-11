a90f0e3bc7215a59c17002f81046e5ef25cb1fb9
Livia Silva <38299821+livribeiro@users.noreply.github.com>
2020-07-11 00:12:22 -0300

    Create PalestranteTest.java

---
+++ Projeto Pratico - BDII/bd2.univas.edu/src/test/java/bd2/edu/univas/si/entities/PalestranteTest.java
@@ -0,0 +1,14 @@
+package bd2.edu.univas.si.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PalestranteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
